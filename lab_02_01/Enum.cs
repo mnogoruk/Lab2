ad9ca4cc6fdb419e30bfcfda80e5dbe14fc9ff15
mnogoruk <lionless072@gmail.com>
2020-04-10 03:01:28 +0300

    init commit!

---
+++ lab_02_01/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab_02_01
+{
+    public enum AccountType { Checking, Deposit };
+    class Enum
+    {
+        static void Main(string[] args)
+        {
+            AccountType goldAccount; 
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+
+        }
+    }
+}
